44405dd9c28898a59ec2356a4f08bddd1f3f6667
Devitar <46533796+Devitar@users.noreply.github.com>
2019-02-08 16:23:08 -0700

    Overhauled website
    Added multiple pages without the need to go to another URL or query string.

---
+++ JavaScript/Main.js
@@ -0,0 +1,5 @@
+// $(document).ready(function () {
+//     $("#Page1").load("../SecondaryHTML.Burgers.html");
+// });
+
+//document.getElementById("Page1").innerHTML='<object type="text/html" data="../SecondaryHTML.Burgers.html" ></object>';
